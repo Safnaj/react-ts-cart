795e1da8068f272537fa3d834a71a13fcc386781
Safnaj <safnaj99@gmail.com>
2021-09-15 19:09:14 +0530

    pre-commit config

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,33 +1,44 @@
 import Button from "@material-ui/core/Button";
+import Item from "../Item/Item";
 import { CartItemType } from "../../models/cart.model";
 import { Wrapper } from "./CartItem.style";
 
 type Props = {
-  item: CartItemType;
-  addToCart: (clickedItem: CartItemType) => void;
-  removeFromCart: (id: number) => void;
+	item: CartItemType;
+	addToCart: (clickedItem: CartItemType) => void;
+	removeFromCart: (id: number) => void;
 };
 
 const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
-  <Wrapper>
-    <div>
-      <h3>{item.title}</h3>
-      <div className='information'>
-        <p>Price: ${item.price}</p>
-        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
-      </div>
-      <div className='buttons'>
-        <Button size='small' disableElevation variant='contained' onClick={() => removeFromCart(item.id)}>
-          -
-        </Button>
-        <p>{item.amount}</p>
-        <Button size='small' disableElevation variant='contained' onClick={() => addToCart(item)}>
-          +
-        </Button>
-      </div>
-    </div>
-    <img src={item.image} alt={item.title} />
-  </Wrapper>
+	<Wrapper>
+		<div>
+			<h3>{item.title}</h3>
+			<div className="information">
+				<p>Price: ${item.price}</p>
+				<p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+			</div>
+			<div className="buttons">
+				<Button
+					size="small"
+					disableElevation
+					variant="contained"
+					onClick={() => removeFromCart(item.id)}
+				>
+					-
+				</Button>
+				<p>{item.amount}</p>
+				<Button
+					size="small"
+					disableElevation
+					variant="contained"
+					onClick={() => addToCart(item)}
+				>
+					+
+				</Button>
+			</div>
+		</div>
+		<img src={item.image} alt={item.title} />
+	</Wrapper>
 );
 
 export default CartItem;
